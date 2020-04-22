ec0c5275f1d8b37590fc8b21d1750aff3235aa3f
47karimasif <karimasif98@gmail.com>
2020-04-22 15:24:00 +0530

    media-query for small-screen

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -5,29 +5,28 @@ const Home = () => {
   return (
     <section className="showcase d-flex align-items-center">
       <div className="row">
-        <div className="col-sm-12 col-md-8 d-flex align-items-center">
-          <div>
+        <div className="col-8 d-flex align-items-center">
+          <div className="showcase-content">
             <h1 className="text-center">
               <span className="title display-3">Covid-19</span>
             </h1>
             <p className="text-center">
-              <span className="h5 text-center quote">
-                Stay at home in quarantine
-              </span>
+              <span className="quote h5">Stay at home in quarantine</span>
             </p>
-            <p className="gray-text px-5">
+            <p className="gray-text px-5 tagline">
               Covid-19 is a contagious respiratory disease caused by the
-              coronavirus. It can cause mild to severe illness and sometimes
-              lead to death.
+              coronavirus.
             </p>
           </div>
         </div>
-        <div className="d-none d-md-block col-md-4">
-          <img
-            src="https://drive.google.com/uc?id=1-zYyyWLBDS1qVkuSAgUVeTA6nSLSCyEU"
-            alt="showcase"
-            className="img-fluid"
-          />
+        <div className="col-4 d-flex align-items-center">
+          <div>
+            <img
+              src="https://drive.google.com/uc?id=1-zYyyWLBDS1qVkuSAgUVeTA6nSLSCyEU"
+              alt="showcase"
+              className="img-fluid"
+            />
+          </div>
         </div>
       </div>
     </section>
