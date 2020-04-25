67437499b3278b0714e13074d9f215245c92c017
47karimasif <karimasif98@gmail.com>
2020-04-26 01:40:57 +0530

    error-fixed

--- src/pages/India.js
+++ src/pages/India.js
@@ -1,5 +1,5 @@
 import React from "react";
-import StatePicker1 from "../componets/India/StatePicker/StatePicker1";
+import StatePicker1 from "../components/India/StatePicker/StatePicker1";
 import "../App.css";
 const India = () => {
   return (
