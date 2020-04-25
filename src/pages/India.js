e9dbcaa65ef212142f30d778859138cf89528d79
47karimasif <karimasif98@gmail.com>
2020-04-26 00:46:38 +0530

    updated

--- src/pages/India.js
+++ src/pages/India.js
@@ -1,14 +1,12 @@
 import React from "react";
 import StatePicker1 from "../componets/india/StatePicker/StatePicker1";
 import "../App.css";
-class India extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <StatePicker1 />
-      </div>
-    );
-  }
-}
+const India = () => {
+  return (
+    <div className="container">
+      <StatePicker1 />
+    </div>
+  );
+};
 
 export default India;
