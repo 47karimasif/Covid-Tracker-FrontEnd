dba868f953b479de25347812d4cbc1770dd407fd
47karimasif <karimasif98@gmail.com>
2020-05-02 20:32:18 +0530

    landing-page updated

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
-import firstImage from '../../Images/Untitled-26.png';
-import './Home.css';
+import React from "react";
+import firstImage from "../../Images/Untitled-26.png";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <section className="home" id="Home">
-            <div className="container home-container">
-                <div className="row">
+  return (
+    <div>
+      <section className="home" id="Home">
+        <div className="container home-container">
+          <div className="row">
+            <div className="col-8 d-flex align-items-center">
+              <div className="home-text">
+                <h1 className=" pr-auto">CORONA</h1>
+                <br />
+                <h4 className=" pr-auto">Virus Disease (COVID-19)</h4>
+                <br />
 
-                    <div className="col-8 d-flex align-items-center">
-                        <div className="home-text">
-                        <h1 className=" pr-auto">CORONA</h1><br />
-                        <h4 className=" pr-auto">Virus Disease  (COVID-19)</h4><br />
-
-                        <p className="lead  pr-auto">
-                         COVID-19 is a new illness that can affect our lungs and airways. It is caused by a virus called the corona virus. The illness first started in Wuhan, Hubei, China.
-
-                            Common signs of the infection include respiratory symtpoms, fever, cough, shortness of breath and breathing difficulty.  </p>    
-                        <br />
-                    </div>
-                </div>
-                
-                    <div className="col-4 d-flex align-items-center">
-                        <img src={firstImage} alt="First Image" className="img-fluid home-image pl-auto" />
-                    </div>
-                </div>
+                <p className="lead  pr-auto">
+                  COVID-19 is a new illness that can affect our lungs and
+                  airways. It is caused by a virus called the corona virus. The
+                  illness first started in Wuhan, Hubei, China. Common signs of
+                  the infection include respiratory symtpoms, fever, cough,
+                  shortness of breath and breathing difficulty.{" "}
+                </p>
+                <br />
+              </div>
             </div>
-        </section>
 
-        <br /><hr /><br />
+            <div className="col-4 d-flex align-items-center">
+              <img
+                src={firstImage}
+                alt="First"
+                className="img-fluid home-image pl-auto"
+              />
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+
+      <br />
+      <hr />
+      <br />
+    </div>
+  );
+};
 
 export default Home;
--- src/components/Landing/SaveLives.jsx
+++ src/components/Landing/SaveLives.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import fifthImage from '../../Images/safe.png'
-import styles from './SaveLives.module.css';
+import React from "react";
+import fifthImage from "../../Images/safe.png";
+import styles from "./SaveLives.module.css";
 
 const SaveLives = () => {
-    return (
-        <div>
-            <section>
-            <div className="container text-center">
-                <img src={fifthImage} alt="img" className={styles.fifthImage} />
-                <h1 className="font-weight-bolder">Stay home. Save Lives.</h1>
-                <h3 className="font-weight-bold">Help stop Corona Virus</h3>
-            </div>
-        </section>
-
-        <br /><br />
+  return (
+    <div>
+      <section>
+        <div className="container text-center">
+          <img src={fifthImage} alt="img" className={styles.fifthImage} />
+          <h1 className="font-weight-bolder">Stay home. Save Lives.</h1>
+          <h3 className="font-weight-bold">Help stop Corona Virus</h3>
         </div>
-    )
-}
+      </section>
+
+      <br />
+      <br />
+    </div>
+  );
+};
 
 export default SaveLives;
